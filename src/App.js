146a40f53923b1e6765aa36d2eb5dbb564b20fc2
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-06-09 11:58:25 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import React from "react";
 import Products from "./components/Products/Products";
+import Header from "./components/Layout/header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Products />
     </div>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -15,6 +15,76 @@ const items = [
     title: "Title of the Item",
     thumbnail: "placeholder.png",
   },
+  {
+    id: 2,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 3,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 4,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 5,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 6,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 7,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 8,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 9,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 10,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
+  {
+    id: 11,
+    discountedPrice: 80,
+    price: 100,
+    title: "Title of the Item",
+    thumbnail: "placeholder.png",
+  },
 ];
 
 const Products = () => {
@@ -23,6 +93,16 @@ const Products = () => {
       <div className={"product-list--wrapper"}>
         <ListItem data={items[0]}></ListItem>
         <ListItem data={items[1]}></ListItem>
+        <ListItem data={items[2]}></ListItem>
+        <ListItem data={items[3]}></ListItem>
+        <ListItem data={items[4]}></ListItem>
+        <ListItem data={items[5]}></ListItem>
+        <ListItem data={items[6]}></ListItem>
+        <ListItem data={items[7]}></ListItem>
+        <ListItem data={items[8]}></ListItem>
+        <ListItem data={items[9]}></ListItem>
+        <ListItem data={items[10]}></ListItem>
+        <ListItem data={items[11]}></ListItem>
       </div>
     </div>
   );
