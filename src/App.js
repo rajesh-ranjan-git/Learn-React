6578e345007517a3d0702cabda2ff8d97a017c84
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-06-09 11:10:38 +0530

    Items modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import ListItem from "./components/ListItem";
 const App = () => {
   return (
     <div tabIndex="" className="" onClick="">
-      <ListItem></ListItem>
-      <ListItem></ListItem>
+      <ListItem
+        data={{
+          discountedPrice: 340,
+          price: 450,
+          title: "Title of the Item",
+          thumbnail: "placeholder.png",
+        }}
+      ></ListItem>
+      <ListItem
+        data={{
+          discountedPrice: 80,
+          price: 100,
+          title: "Title of the Item",
+          thumbnail: "placeholder.png",
+        }}
+      ></ListItem>
     </div>
   );
 
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,20 +1,24 @@
-import AddToCartIcon from "../assets/icons/cart.jpg";
+import AddToCartIcon from "../assets/icons/add_cart.svg";
 
-const ListItem = () => {
+const ListItem = ({ data }) => {
   return (
-    <div>
-      <img src="/assets/placeholder.png" alt="Some Title" />
-      <div>
-        <div>
-          <span>₹340</span>
+    <div className={"item-card"}>
+      <img
+        className={"item-fluid"}
+        src={`/assets/${data.thumbnail}`}
+        alt="Some Title"
+      />
+      <div className={"item-card__information"}>
+        <div className={"pricing"}>
+          <span>₹{data.discountedPrice}</span>
           <small>
-            <strike>₹450</strike>
+            <strike>₹{data.price}</strike>
           </small>
         </div>
-        <div>
-          <h3>TItle of the Item</h3>
+        <div className={"title"}>
+          <h3>{data.title}</h3>
         </div>
-        <button>
+        <button className={"cart-add"}>
           <span>Add to Cart</span>
           <img src={AddToCartIcon} alt="CartIcon" />
         </button>
